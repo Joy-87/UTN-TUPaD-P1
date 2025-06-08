01b88a9830bb6d5d73fb22732344dd0b7d568f5b
joana-lopez <jl105658@gmail.com>
2025-06-08 10:20:36 -0300

    actividades de Listas

--- Listas/Actividad_2.py
+++ Listas/Actividad_2.py
@@ -1,3 +1,3 @@
 mi_lista= ['manzana', 'banana', 'arandanos', "frutilla", "cereza"]
-penultimo_elemento=mi_lista[-2]
+penultimo_elemento=mi_lista[-2] #colocamos entre corchetes [-2] para indicar que queremos extraer el penultimo elemento de la lista
 print(penultimo_elemento)
--- Listas/Actividad_5.py
+++ Listas/Actividad_5.py
@@ -1,3 +1,5 @@
 numeros=[8, 15, 3, 22, 7]
 numeros.remove(max(numeros))
 print(numeros)
+
+#max busca el valor más alto dentro de la lista y lo elimina, mostrando en pantalla 8, 3, 15, 7
